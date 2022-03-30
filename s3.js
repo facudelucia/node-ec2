0718dd448de776757fda5b9111bec9cce183fe7c
facudelucia <facundodeluciajudo@gmail.com>
2022-03-30 00:20:35 -0300

    added env variables

--- s3.js
+++ s3.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
 
-const bucketName = "node-ec2-image-upload"
-const region = "us-east-1"
-const accessKeyId = "AKIAR6WNJNHJHPESPW5K"
-const secretAccessKey = "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"
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
 
 const s3 = new S3({
   region,
